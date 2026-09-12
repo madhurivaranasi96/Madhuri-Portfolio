711a0f638c8c5363f59a8b8792392e3be4b8463d
madhurivaranasi96 <madhuri.varanasi96@gmail.com>
2026-09-12 18:27:21 +0530

    Added theme

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +7,24 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
-export class Navbar {
+export class Navbar implements OnInit {
+
+  isLightTheme = false;
+
+  ngOnInit(): void {
+    this.isLightTheme = localStorage.getItem('portfolio-theme') === 'light';
+    this.applyTheme();
+  }
+
+  toggleTheme(): void {
+    this.isLightTheme = !this.isLightTheme;
+    localStorage.setItem('portfolio-theme', this.isLightTheme ? 'light' : 'dark');
+    this.applyTheme();
+  }
+
+  private applyTheme(): void {
+    document.body.classList.toggle('light-theme', this.isLightTheme);
+  }
 
   scrollToSection(sectionId: string): void {
 
